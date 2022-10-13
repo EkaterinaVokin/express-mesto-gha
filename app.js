6408544c4717937244f583e7e222e377ea1812c2
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-10-13 20:49:30 +0700

    feat: Реализовать временное решение авторизации

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ app.use(express.json());
 
 const { PORT = 3000 } = process.env;
 
-app.get('/', (req, res) => {
-  res.send('привет');
+app.use((req, res, next) => {
+  req.user = {
+    _id: '63480ba1154a2b3491ed58c8',
+  };
+
+  next();
 });
+
+app.use('/', require('./routes/users'));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
