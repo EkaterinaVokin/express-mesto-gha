235fba562ed13098b9fcc8c5335c4b901123833d
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-10-13 17:04:54 +0700

    feat: Установить порт

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+
+const { PORT = 3000 } = process.env;
+app.listen(PORT, () => {
+  console.log('Ссылка на сервер');
+});
