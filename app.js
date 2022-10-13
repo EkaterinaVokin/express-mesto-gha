10e1714e686ab17d284e9f96cc89f6503ef39fe0
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-10-13 17:06:19 +0700

    feat: Подключить middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.use(express.json());
 const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/mestodb');
 app.listen(PORT, () => {
