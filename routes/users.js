04712b0352fbe4f02c7e06478f5c8173d92fa5ac
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-10-14 15:26:21 +0700

    feat: Добавить роутеры пользователю и карточкам

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
-const { getUsers, getUserById, createUser } = require('../controllers/users');
+const {
+  getUsers, getUserById, createUser, updateProfile, updateAvatar,
+} = require('../controllers/users');
 
-router.get('/users', getUsers);
-router.get('/users/:userId', getUserById);
-router.post('/users', createUser);
+router.get('/users', getUsers); // возвращает всех пользователей
+router.get('/users/:userId', getUserById); // возвращает пользователя по _id
+router.post('/users', createUser); // создаёт пользователя
+router.patch('/users/me', updateProfile); // обновляет профиль
+router.patch('/users/me/avatar', updateAvatar); // обновляет аватар
 
 module.exports = router;
