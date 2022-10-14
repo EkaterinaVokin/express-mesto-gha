a8ca37ed07a206604d75a326cdba8c73cf966f1c
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-10-14 13:22:37 +0700

    feat: Добавить VersionKey для схемы карточек

--- models/card.js
+++ models/card.js
@@ -2,36 +2,39 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 // создать схему для карточек
-const cardSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
-  },
-  link: {
-    type: String,
-    required: true,
-    validate: (value) => {
-      if (validator.isURL(value)) {
-        return true;
-      }
-      return false;
+const cardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+      validate: (value) => {
+        if (validator.isURL(value)) {
+          return true;
+        }
+        return false;
+      },
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId, // тип ObjectId
+      ref: 'user', // ссылка на модель автора карточки
+      required: true,
+    },
+    likes: {
+      type: [mongoose.Schema.Types.ObjectId], // массив ObjectId
+      default: () => [],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
     },
   },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId, // тип ObjectId
-    ref: 'user', // ссылка на модель автора карточки
-    required: true,
-  },
-  likes: {
-    type: [mongoose.Schema.Types.ObjectId], // массив ObjectId
-    default: () => [],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { versionKey: false },
+);
 
 module.exports = mongoose.model('card', cardSchema); // создать модель для карточек
