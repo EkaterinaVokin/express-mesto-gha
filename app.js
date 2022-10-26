958bddacef9b9a58d960bdb62a385081ab4a74c6
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-10-26 11:53:11 +0700

    feat: защитить роуты

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const helmet = require('helmet'); // модуль для защиты приложения известных веб-уязвимостей
 const { ERROR_NOT_FOUND } = require('./constants');
 const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const app = express();
 
@@ -12,16 +13,19 @@ app.use(helmet());
 
 const { PORT = 3000 } = process.env;
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '63480ba1154a2b3491ed58c8',
-  };
+// app.use((req, res, next) => {
+//   req.user = {
+//     _id: '63480ba1154a2b3491ed58c8',
+//   };
 
-  next();
-});
+//   next();
+// });
 
 app.post('/signin', login); // авторизация
 app.post('/signup', createUser); // регистрация
+
+// авторизация
+app.use(auth);
 
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
