3bd8692d11fea7c4cb585491c9f3f2be0f50b656
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-10-27 14:41:33 +0700

    feat: Удалить ненужный роут

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,6 @@ const {
 router.get('/users', getUsers); // возвращает всех пользователей
 router.get('/users/me', getMe); // возвращает пользователя
 router.get('/users/:userId', getUserById); // возвращает пользователя по _id
-// router.post('/users', createUser); // создаёт пользователя
 router.patch('/users/me', updateProfile); // обновляет профиль
 router.patch('/users/me/avatar', updateAvatar); // обновляет аватар
 
